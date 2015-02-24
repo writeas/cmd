2c1bcd7303a0cf6298e842631f5d1bb99afa5c01
Matt Baer <matt@flashstuf.com>
2015-02-23 19:59:06 -0500

    Ability to use database store
    Uses MySQL. Still allows file-based storage with -o flag, or database
    storage with WA_USER, WA_PASSWORD, WA_HOST, and WA_DB env variables.

--- cmd.go
+++ cmd.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"database/sql"
 	"flag"
-	"strings"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
 	"net/http"
-	
+	"os"
+	"strings"
+
 	"github.com/writeas/writeas-telnet/store"
 )
 
@@ -14,6 +17,7 @@ var (
 	outDir string
 	indexPage []byte
 	debugging bool
+	db        *sql.DB
 )
 
 func poster(w http.ResponseWriter, r *http.Request) {
@@ -24,25 +28,37 @@ func poster(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	filename, err := store.SavePost(outDir, []byte(post))
+	var friendlyId string
+	var err error
+	if outDir != "" {
+		// Using file-based storage
+		friendlyId, err = store.SavePost(outDir, []byte(post))
+	} else {
+		// Using database storage
+		friendlyId = store.GenerateFriendlyRandomString(store.FriendlyIdLen)
+		editToken := store.Generate62RandomString(32)
+
+		_, err = db.Exec("INSERT INTO posts (id, content, modify_token) VALUES (?, ?, ?)", friendlyId, post, editToken)
+	}
 	if err != nil {
-		fmt.Println(err)
+		fmt.Printf("Error saving: %s\n", err)
 		fmt.Fprint(w, "Couldn't save :(\n")
 		return
 	}
 
 	if debugging {
-		fmt.Printf("Saved new post %s\n", filename)
+		fmt.Printf("Saved new post %s\n", friendlyId)
 	}
 
-	fmt.Fprintf(w, "https://write.as/%s", filename)
+	fmt.Fprintf(w, "https://write.as/%s", friendlyId)
+
 	if !strings.Contains(r.UserAgent(), "Android") {
 		fmt.Fprint(w, "\n")
 	}
 }
 
 func main() {
-	outDirPtr := flag.String("o", "/var/write", "Directory where text files will be stored.")
+	outDirPtr := flag.String("o", "", "Directory where text files will be stored.")
 	staticDirPtr := flag.String("s", "./static", "Directory where required static files exist.")
 	portPtr := flag.Int("p", 8080, "Port to listen on.")
 	debugPtr := flag.Bool("debug", false, "Enables garrulous debug logging.")
@@ -58,6 +74,29 @@ func main() {
 		fmt.Println(err)
 	}
 	fmt.Println("DONE")
+
+	// Connect to database
+	dbUser := os.Getenv("WA_USER")
+	dbPassword := os.Getenv("WA_PASSWORD")
+	dbName := os.Getenv("WA_DB")
+	dbHost := os.Getenv("WA_HOST")
+
+	if outDir == "" && (dbUser == "" || dbPassword == "" || dbName == "") {
+		// Ensure parameters needed for storage (file or database) are given
+		fmt.Println("Database user, password, or database name not set.")
+		return
+	}
+
+	if outDir == "" {
+		fmt.Print("Connecting to database...")
+		db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4", dbUser, dbPassword, dbHost, dbName))
+		if err != nil {
+			fmt.Printf("\n%s\n", err)
+			return
+		}
+		defer db.Close()
+		fmt.Println("CONNECTED")
+	}
 
 	fmt.Printf("Serving on http://localhost:%d\n", *portPtr)
 
